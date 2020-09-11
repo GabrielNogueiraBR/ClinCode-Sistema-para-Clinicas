18bdd94b0e24e69d5e3530f6b92200bdb6264241
Vitor Gois <vitorcgois777@gmail.com>
2020-09-11 18:38:43 -0300

    Criação da Classe Pessoa/Endereço

---
+++ back/clinCode-Java/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Pessoa p = new Pessoa("Vitor", "07/12/2001", "53.854.279-2", "488.668.868-31", "vitorcgois777@gmail.com", "Albertina de Jesus", "Jardim Colina", 18, "Pilar do Sul", "São Paulo");
+
+        System.out.println(p);
+
+    }
+}
