be1d97aeb3e9297a634cb982d54f0a0cfa2d4e78
Gabriel Nogueira <gabrielnoog7@gmail.com>
2020-09-29 17:48:31 -0300

    create Spring Boot project

---
+++ clincode/src/test/java/com/api/clincode/ClincodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.clincode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClincodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
