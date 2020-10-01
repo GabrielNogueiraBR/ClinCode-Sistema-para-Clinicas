e8370b1bee793a8f2056f232b7d6b961eb20b3c9
Gabriel Nogueira <gabrielnoog7@gmail.com>
2020-09-30 22:20:13 -0300

    Atualização de Classes

--- clincode/src/main/java/com/api/clincode/entity/UsuarioEntity.java
+++ clincode/src/main/java/com/api/clincode/entity/UsuarioEntity.java
@@ -1,5 +1,7 @@
 package com.api.clincode.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,8 +12,12 @@ import javax.persistence.Id;
 //Descrição: Essa classe vai conter as informações de login de cada usuário. 
 //Vai buscar no BD as informaçoes DE ACORDO com os privilégio (se é medico, paciente, atendente ou admin) e construir a o objeto de acordo com essas informações.
 @Entity
-public class UsuarioEntity{
+public class UsuarioEntity implements Serializable{
     
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id_usuario;
--- clincode/src/main/java/com/api/clincode/model/Administrador.java
+++ clincode/src/main/java/com/api/clincode/model/Administrador.java
@@ -2,11 +2,9 @@ package com.api.clincode.model;
 
 //@autor: Gabriel Nogueira 
 //Descrição: Essa classe vai conter os atributos e métodos específicos da classe administrador.
-public class Administrador extends Pessoa{
+public class Administrador {
 
 
-    public Administrador(final String nome, final String dataNascimento, final String rg, final String cpf, final String email, final String telefone, final String rua, final String bairro, final int num, final String cidade, final String estado) {
-        super(nome, dataNascimento, rg, cpf, email, telefone, rua, bairro, num, cidade, estado);
-    }
+  
     
 }
