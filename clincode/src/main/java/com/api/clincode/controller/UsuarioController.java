623dac83d7b145f0fc539ac7f24e27e9d80c9517
Gabriel Nogueira <gabrielnoog7@gmail.com>
2020-10-01 19:45:27 -0300

    Criação da Classe ViewController para a navegação do Projeto e organização de arquivos .css

--- clincode/src/main/java/com/api/clincode/controller/UsuarioController.java
+++ clincode/src/main/java/com/api/clincode/controller/UsuarioController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -37,5 +38,9 @@ public class UsuarioController {
         return ResponseEntity.created(uri).build();
     }
     
-
+    @GetMapping("/login")
+    public ModelAndView redirecionaLogin(){
+        ModelAndView mv = new ModelAndView("loginPaciente");
+        return mv;
+    }
 }
