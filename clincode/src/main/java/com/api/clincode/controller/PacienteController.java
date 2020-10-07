50dcdfe4ab64877b54850a546378444c7c57a2c8
Gabriel Nogueira <gabrielnoog7@gmail.com>
2020-10-07 15:10:04 -0300

    Correção de erro ao buscar ID que não existe no BD
    Implementado o retorno do tipo NOT FOUND.

--- clincode/src/main/java/com/api/clincode/controller/PacienteController.java
+++ clincode/src/main/java/com/api/clincode/controller/PacienteController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -41,7 +42,7 @@ public class PacienteController {
     }
 
     @PostMapping()
-    public ResponseEntity<Void> postPaciente(@RequestBody PacienteEntity entity){
+    public ResponseEntity<Void> postPaciente(@ModelAttribute PacienteEntity entity){
         entity = service.salvarPaciente(entity);
 
         if(entity != null)
