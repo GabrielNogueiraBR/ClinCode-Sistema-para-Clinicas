decca045571151510af318eaea8296ff91cd8348
Gabriel Nogueira <gabrielnoog7@gmail.com>
2020-11-25 21:55:25 -0300

    Retirando imports desnecessários e  atualização de Voltar

--- clincode/src/main/java/com/api/clincode/controller/LoginController.java
+++ clincode/src/main/java/com/api/clincode/controller/LoginController.java
@@ -10,12 +10,10 @@ import com.api.clincode.service.MedicoService;
 import com.api.clincode.service.PacienteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
